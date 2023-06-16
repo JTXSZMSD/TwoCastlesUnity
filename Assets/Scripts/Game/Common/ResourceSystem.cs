9f949cd4ce3def1bef04da624f2c70bae4fecd80
JTXSZMSD <91826944+JTXSZMSD@users.noreply.github.com>
2023-06-16 12:23:32 +0300

    ResourceSystems

---
+++ Assets/Scripts/Game/Common/ResourceSystem.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceSystem : MonoBehaviour
+{
+
+    public uint Gold;
+    public uint Lumber;
+    public uint Iron;
+    public uint Stone;
+    public uint People;
+    public uint PeopleLimit;
+    public uint Horses;
+    public uint ArmyLimit;
+    public uint WallsLimit;
+    public uint Powder;
+    public uint ResourceLimit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+}
