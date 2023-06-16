02289cc4e8552d74968dcc50242d605f1b94becf
JTXSZMSD <91826944+JTXSZMSD@users.noreply.github.com>
2023-06-16 12:04:31 +0300

    PlayerTurnButtonsMainInteractions

---
+++ Assets/Scripts/Game/PlayerTurn/FirstStepButtons.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstStepButtons : MonoBehaviour
+{
+    public void pushBuildingsButton()
+    {
+
+    }
+
+    public void pushArmyButton()
+    {
+
+    }
+
+    public void pushMarketButton()
+    {
+
+    }
+
+    public void pushMagicButton()
+    {
+
+    }
+
+    public void pushContinueButton()
+    {
+
+    }
+}
