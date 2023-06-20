bbac3832d10300d92337ef11b25e7aa155674fad
JTXSZMSD <91826944+JTXSZMSD@users.noreply.github.com>
2023-06-20 17:52:23 +0300

    Building and Resource systems in progress

--- Assets/Scripts/Game/Common/BuildingsSystem.cs
+++ Assets/Scripts/Game/Common/BuildingsSystem.cs
@@ -4,5 +4,87 @@ using UnityEngine;
 
 public class BuildingsSystem : MonoBehaviour
 {
+    public uint GoldMineLevel = 1;
+    public uint LumberMillLevel = 1;
+    public uint IronMineLevel = 1;
+    public uint StoneQuarryLevel = 1;
+    public uint StorehouseLevel = 1;
+    public uint HousesLevel = 1;
+    public uint BarracksLevel = 1;
+    public uint WallsLevel = 1;
+    public uint StableLevel = 1;
 
+    public void UpgradeGoldMine()
+    {
+        uint GoldCost = GoldMineLevel * 20;
+        uint IronCost = GoldMineLevel * 20;
+        uint LumberCost = GoldMineLevel * 20;
+
+    }
+
+    public void UpgradeLumberMill() 
+    {
+        uint GoldCost = LumberMillLevel * 20;
+        uint IronCost = LumberMillLevel * 20;
+        uint LumberCost = LumberMillLevel * 20;
+        uint StoneCost = LumberMillLevel * 20;
+
+    }
+
+    public void UpgradeIronMine() 
+    {
+        uint GoldCost = IronMineLevel * 20;
+        uint IronCost = IronMineLevel * 20;
+        uint LumberCost = IronMineLevel * 20;
+
+    }
+
+    public void UpgradeStoneQuarry() 
+    {
+        uint GoldCost = StoneQuarryLevel * 20;
+        uint IronCost = StoneQuarryLevel * 20;
+        uint LumberCost = StoneQuarryLevel * 20;
+
+    }
+
+    public void UpgradeStorehouse() 
+    {
+        uint GoldCost = StorehouseLevel * 20;
+        uint IronCost = StorehouseLevel * 20;
+        uint LumberCost = StorehouseLevel * 20;
+        uint StoneCost = StorehouseLevel * 20;
+    }
+
+    public void UpgradeHouses() 
+    {
+        uint GoldCost = HousesLevel * 20;
+        uint IronCost = HousesLevel * 20;
+        uint LumberCost = HousesLevel * 20;
+        uint StoneCost = HousesLevel * 20;
+    }
+
+    public void UpgradeBarracks() 
+    {
+        uint GoldCost = BarracksLevel * 20;
+        uint IronCost = BarracksLevel * 20;
+        uint LumberCost = BarracksLevel * 20;
+        uint StoneCost = BarracksLevel * 20;
+    }
+
+    public void UpgradeWalls() 
+    {
+        uint GoldCost = WallsLevel * 20;
+        uint IronCost = WallsLevel * 20;
+        uint LumberCost = WallsLevel * 20;
+        uint StoneCost = WallsLevel * 60;
+
+    }
+
+    public void UpgradeStable() 
+    {
+        uint GoldCost = StableLevel * 20;
+        uint IronCost = StableLevel * 20;
+        uint LumberCost = StableLevel * 20;
+        uint StoneCost = StableLevel * 20;
+    }
 }
--- Assets/Scripts/Game/Common/ResourceSystem.cs
+++ Assets/Scripts/Game/Common/ResourceSystem.cs
@@ -5,21 +5,66 @@ using UnityEngine;
 public class ResourceSystem : MonoBehaviour
 {
 
-    public uint Gold;
-    public uint Lumber;
-    public uint Iron;
-    public uint Stone;
-    public uint People;
-    public uint PeopleLimit;
-    public uint Horses;
-    public uint ArmyLimit;
-    public uint WallsLimit;
-    public uint Powder;
-    public uint ResourceLimit;
+    public uint Gold = 0;
+    public uint Lumber = 0;
+    public uint Iron = 0;
+    public uint Stone = 0;
+    public uint People = 0;
+    public uint PeopleLimit = 10;
+    public uint Horses = 5;
+    public uint ArmyLimit = 10;
+    public uint WallsLimit = 10;
+    public uint Powder = 0;
+    public uint ResourceLimit = 500;
 
-    // Start is called before the first frame update
-    void Start()
+    public byte GoldMineImprovementTurn = 0;
+    public byte LumberMillImprovementTurn = 0;
+    public byte IronMineImprovementTurn = 0;
+    public byte StoneQuarryImprovementTurn = 0;
+
+    public void income()
     {
+        GoldIncome();
+        LumberIncome();
+        IronIncome();
+        StoneIncome();
+        PeopleIncome();
+    }
+
+    private void GoldIncome()
+    {
+        //if (GoldMineImprovementTurn > 0)
+        //{
+        //    Gold += (BuildingSystem.GoldMineLevel * 30 + 140) * 1.25;
+        //    GMIturn -= 1;
+        //}
+        //else
+        //{
+        //    PlayerGold += PlayerGMlvl * 30 + 140;
+        //}
+        //if (PlayerGold > PlayerResourceLimit)
+        //{
+        //    PlayerGold = PlayerResourceLimit;
+        //}
+    }
+
+    private void LumberIncome()
+    {
+
+    }
+
+    private void IronIncome()
+    {
+
+    }
+
+    private void StoneIncome() 
+    { 
         
+    }
+
+    private void PeopleIncome()
+    {
+
     }
 }
