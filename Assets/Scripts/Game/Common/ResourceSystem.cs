57937cf33b18dd7de5d5e8ec9e1578b28d351158
JTXSZMSD <91826944+JTXSZMSD@users.noreply.github.com>
2023-06-20 18:36:16 +0300

    Income WORKING

--- Assets/Scripts/Game/Common/ResourceSystem.cs
+++ Assets/Scripts/Game/Common/ResourceSystem.cs
@@ -1,9 +1,11 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ResourceSystem : MonoBehaviour
 {
+    public BuildingsSystem Buildings;
 
     public uint Gold = 0;
     public uint Lumber = 0;
@@ -24,47 +26,78 @@ public class ResourceSystem : MonoBehaviour
 
     public void income()
     {
-        GoldIncome();
-        LumberIncome();
-        IronIncome();
-        StoneIncome();
-        PeopleIncome();
+        //Gold
+        Gold += GoldIncome();
+        if (Gold > ResourceLimit)
+        {
+            Gold = ResourceLimit;
+        }
+
+        //    //Lumber
+        //    Lumber += LumberIncome();
+        //    if (Lumber > ResourceLimit) 
+        //    { 
+        //        Lumber = ResourceLimit;
+        //    }
+
+        //    //Iron
+        //    Iron += IronIncome();
+        //    if (Iron > ResourceLimit) 
+        //    { 
+        //        Iron = ResourceLimit;
+        //    }
+
+        //    //Stone
+        //    Stone += StoneIncome();
+        //    if (Stone > ResourceLimit)
+        //    { 
+        //        Stone = ResourceLimit; 
+        //    }
+
+        //    //People
+        //    People += PeopleIncome();
+        //    if (People > PeopleLimit)
+        //    {
+        //        People = PeopleLimit;
+        //    }
     }
 
-    private void GoldIncome()
+    private uint GoldIncome()
     {
-        //if (GoldMineImprovementTurn > 0)
-        //{
-        //    Gold += (BuildingSystem.GoldMineLevel * 30 + 140) * 1.25;
-        //    GMIturn -= 1;
-        //}
-        //else
-        //{
-        //    PlayerGold += PlayerGMlvl * 30 + 140;
-        //}
-        //if (PlayerGold > PlayerResourceLimit)
-        //{
-        //    PlayerGold = PlayerResourceLimit;
-        //}
+        uint temp;
+        if (GoldMineImprovementTurn > 0)
+        {
+            temp = Convert.ToUInt32((Buildings.GoldMineLevel * 30 + 140) * 1.25);
+            GoldMineImprovementTurn -= 1;
+        }
+        else
+        {
+            temp = Buildings.GoldMineLevel * 30 + 140;
+        }
+        return temp;
     }
 
-    private void LumberIncome()
-    {
+    //private uint LumberIncome()
+    //{
+    //    uint temp;
+    //    return temp;
+    //}
 
-    }
+    //private uint IronIncome()
+    //{
+    //    uint temp;
+    //    return temp;
+    //}
 
-    private void IronIncome()
-    {
+    //private uint StoneIncome() 
+    //{
+    //    uint temp;
+    //    return temp;
+    //}
 
-    }
-
-    private void StoneIncome() 
-    { 
-        
-    }
-
-    private void PeopleIncome()
-    {
-
-    }
+    //private uint PeopleIncome()
+    //{
+    //    uint temp;
+    //    return temp;
+    //}
 }
